1b51d610f8bc14eb2667d1ed0eae4b08da5feb49
oren12321 <oren12321@gmail.com>
2024-02-25 01:52:59 +0200

    Add numeric array concepts and add numeric and complex matrix transpose functions

--- include/oc/ext/linalg.h
+++ include/oc/ext/linalg.h
@@ -1,5 +1,56 @@
 #ifndef OC_EXT_LINALG_H
 #define OC_EXT_LINALG_H
 
-#endif // OC_EXT_LINALG_H
+#include <type_traits>
+#include <complex>
+#include <algorithm>
 
+#include <oc/arrnd.h>
+
+namespace oc::linalg {
+namespace details {
+    template <typename T>
+    concept numeric_arrnd_complient = oc::arrnd_complient<T> && std::is_arithmetic_v<typename T::value_type>;
+
+    template <template <typename...> typename T, typename... Args>
+    [[nodiscard]] static inline constexpr std::true_type is_template_type_impl(T<Args...>)
+    {
+        return std::true_type;
+    }
+    template <template <typename...> typename T>
+    [[nodiscard]] static inline constexpr std::false_type is_template_type_impl(...)
+    {
+        return std::false_type;
+    }
+    template <template <typename...> typename T, typename U>
+    using is_template_type = decltype(is_template_type_impl<T>(std::declval<typename std::decay_t<U>>()));
+
+    template <typename T>
+    concept complex_arrnd_complient = oc::arrnd_complient<T> && is_template_type<std::complex, typename T::value_type>::value;
+
+    template <numeric_arrnd_complient ArCo>
+    ArCo transpose(const ArCo& mat)
+    {
+        assert(mat.header().dims().size() == 2);
+        return oc::transpose(mat, {1, 0});
+    }
+
+    template <complex_arrnd_complient ArCo>
+    ArCo transpose(const ArCo& mat)
+    {
+        assert(mat.header().dims().size() == 2);
+        auto tmat = oc::transpose(mat, {1, 0});
+        std::for_each(tmat.begin(), tmat.end(), [](auto& c) {
+            c.imag(-c.imag());
+        });
+        return tmat;
+    }
+}
+
+using details::numeric_arrnd_complient;
+using details::complex_arrnd_complient;
+
+using details::transpose;
+}
+
+#endif // OC_EXT_LINALG_H
--- tests/ext/linalg.cpp
+++ tests/ext/linalg.cpp
@@ -1,4 +1,36 @@
 #include <gtest/gtest.h>
 
+#include <complex>
+
+#include <oc/arrnd.h>
 #include <oc/ext/linalg.h>
 
+TEST(linalg_test, can_check_if_array_type_is_numeric_or_complex_at_compile_time)
+{
+    oc::arrnd<int>{}; // required due to an MSVC compiler issue
+    oc::arrnd<double>{};
+
+    static_assert(oc::linalg::numeric_arrnd_complient<oc::arrnd<int>>);
+    static_assert(oc::linalg::numeric_arrnd_complient<oc::arrnd<double>>);
+
+    oc::arrnd<std::complex<int>>{}; // required due to an MSVC compiler issue
+    oc::arrnd<std::complex<double>>{};
+
+    static_assert(oc::linalg::complex_arrnd_complient<oc::arrnd<std::complex<int>>>);
+    static_assert(oc::linalg::complex_arrnd_complient<oc::arrnd<std::complex<double>>>);
+}
+
+TEST(linalg_test, can_calculate_transposed_matrix)
+{
+    oc::arrnd<int> mat({2, 3}, {1, 2, 3, 4, 5, 6});
+    oc::arrnd<int> rmat({3, 2}, {1, 4, 2, 5, 3, 6});
+
+    EXPECT_TRUE(oc::all_equal(rmat, oc::linalg::transpose(mat)));
+
+    using namespace std::complex_literals;
+
+    oc::arrnd<std::complex<double>> cmat({2, 2}, {-1i, 2.0 + 1i, 4.0 + 2i, -2i});
+    oc::arrnd<std::complex<double>> rcmat({2, 2}, {1i, 4.0 - 2i, 2.0 - 1i, 2i});
+
+    EXPECT_TRUE(oc::all_equal(rcmat, oc::linalg::transpose(cmat)));
+}
