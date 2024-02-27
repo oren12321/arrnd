c66c66e7e5a6e8e35c246f2c8194cad68c961410
oren12321 <oren12321@gmail.com>
2024-02-27 17:46:15 +0200

    Add transpose function to linalg.h file

--- include/oc/ext/linalg.h
+++ include/oc/ext/linalg.h
@@ -1,5 +1,66 @@
 #ifndef OC_EXT_LINALG_H
 #define OC_EXT_LINALG_H
 
-#endif // OC_EXT_LINALG_H
+#include <cstdint>
+#include <type_traits>
+#include <complex>
+#include <algorithm>
+#include <iterator>
 
+#include <oc/arrnd.h>
+
+namespace oc::linalg {
+namespace details {
+    template <oc::arrnd_complient_with_trait<std::is_arithmetic> ArCo>
+    void recursive_transpose(const ArCo& src, ArCo dst)
+    {
+        if (src.header().dims().size() == 2) {
+            auto tsrc = src.transpose({1, 0});
+            dst.copy_from(tsrc);
+            return;
+        }
+
+        typename ArCo::size_type first_dim = src.header().dims().front();
+        for (typename ArCo::size_type i = 0; i < first_dim; ++i) {
+            recursive_transpose(src[interval<std::int64_t>{i, i}], dst[interval<std::int64_t>{i, i}]);
+        }
+    }
+
+    template <oc::arrnd_complient_of_template_type<std::complex> ArCo>
+    void recursive_transpose(const ArCo& src, ArCo dst)
+    {
+        if (src.header().dims().size() == 2) {
+            auto tsrc = src.transpose({1, 0});
+            std::for_each(tsrc.begin(), tsrc.end(), [](auto& c) {
+                c.imag(-c.imag());
+            });
+            dst.copy_from(tsrc);
+            return;
+        }
+
+        typename ArCo::size_type first_dim = src.header().dims().front();
+        for (typename ArCo::size_type i = 0; i < first_dim; ++i) {
+            recursive_transpose(src[interval<std::int64_t>{i, i}], dst[interval<std::int64_t>{i, i}]);
+        }
+    }
+
+    // equivalent to MATLAB's transpose, ctranspose, pagetranspose and pagectranspose functions
+    template <oc::arrnd_complient ArCo>
+    ArCo transpose(const ArCo& mat)
+    {
+        auto ndims = mat.header().dims().size();
+        assert(ndims >= 2);
+
+        auto dims = mat.header().dims();
+        std::swap(dims[ndims - 2], dims[ndims - 1]);
+
+        ArCo res(dims.cbegin(), dims.cend());
+        recursive_transpose(mat, res);
+        return res;
+    }
+}
+
+using details::transpose;
+}
+
+#endif // OC_EXT_LINALG_H
--- tests/ext/linalg.cpp
+++ tests/ext/linalg.cpp
@@ -1,4 +1,25 @@
 #include <gtest/gtest.h>
 
+#include <complex>
+
+#include <oc/arrnd.h>
 #include <oc/ext/linalg.h>
 
+TEST(linalg_test, can_calculate_transposed_matrix)
+{
+    oc::arrnd<int> mat({1, 3, 2, 3}, {1, 1, 1, 2, 2, 2, 2, 2, 2, 4, 4, 4, 3, 3, 3, 6, 6, 6});
+    oc::arrnd<int> rmat({1, 3, 3, 2}, {1, 2, 1, 2, 1, 2, 2, 4, 2, 4, 2, 4, 3, 6, 3, 6, 3, 6});
+
+    EXPECT_TRUE(oc::all_equal(rmat, oc::linalg::transpose(mat)));
+
+    using namespace std::complex_literals;
+
+    oc::arrnd<std::complex<double>> cmat({3, 1, 2, 3},
+        {1.0 + 1i, 1.0 + 1i, 1.0 + 1i, 2.0 + 1i, 2.0 + 1i, 2.0 + 1i, 2.0 + 1i, 2.0 + 1i, 2.0 + 1i, 4.0 + 1i, 4.0 + 1i,
+            4.0 + 1i, 3.0 + 1i, 3.0 + 1i, 3.0 + 1i, 6.0 + 1i, 6.0 + 1i, 6.0 + 1i});
+    oc::arrnd<std::complex<double>> rcmat({3, 1, 3, 2},
+        {1.0 - 1i, 2.0 - 1i, 1.0 - 1i, 2.0 - 1i, 1.0 - 1i, 2.0 - 1i, 2.0 - 1i, 4.0 - 1i, 2.0 - 1i, 4.0 - 1i, 2.0 - 1i,
+            4.0 - 1i, 3.0 - 1i, 6.0 - 1i, 3.0 - 1i, 6.0 - 1i, 3.0 - 1i, 6.0 - 1i});
+
+    EXPECT_TRUE(oc::all_equal(rcmat, oc::linalg::transpose(cmat)));
+}
